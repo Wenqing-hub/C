e8fb9569d75fba3927fb40d110f6222be2fdbf1f
Wenqing <110180856+Wenqing-hub@users.noreply.github.com>
2023-02-19 15:15:10 +0800

    Revert "patch 02"

--- job_one/job8/calculateAge.c
+++
@@ -1,5 +0,0 @@
-
-int calculateAge(int year) {
-  const int CURRENT_YEAR = 2020;
-  return CURRENT_YEAR - year;
-}
--- job_one/job8/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "calculateAge.h"
-
-
-int main(void) {
-  printf("%u", calculateAge(1983));
-}
